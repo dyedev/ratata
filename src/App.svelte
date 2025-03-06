feb6b664f0f04e8fd26737654382a8060941c0b6
Theo Paris <theo@tinted.dev>
2025-03-06 13:53:01 -0800

    feat: add instance card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import InstanceCard from "./InstanceCard.svelte";
 </script>
 
 <div class="flex flex-col md:flex-row h-screen bg-zinc-950">
@@ -252,136 +253,10 @@
         </svg>
       </button>
     </div>
-    <div
-      class="grid md:grid-cols-2 gap-4 bg-zinc-900 border border-zinc-800 p-2 md:p-4 rounded-lg hover:bg-zinc-800 hover:border-zinc-600 transition-colors duration-200 ease-in-out cursor-pointer"
-    >
-      <div class="flex items-center mb-2 pt-2 pl-2.5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="h-4 w-4 text-[#58a6ff] mr-3"
-        >
-          <path
-            d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16"
-          ></path>
-        </svg>
-        <div class="ml-2">
-          <h2 class="text-white font-semibold">
-            Development
-            <div
-              class="inline-flex items-center rounded-full border transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent text-secondary-foreground hover:bg-secondary/80 ml-1.5 py-0 bg-green-500 px-1 text-[8px] pointer-events-none font-bold"
-            >
-              RUNNING
-            </div>
-            <div
-              class="inline-flex items-center rounded-full border transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-sky-300 py-0 px-1 ml-1.5 text-[8px] text-sky-900 pointer-events-none font-bold"
-            >
-              CONTAINER
-            </div>
-          </h2>
-          <p class="text-zinc-400 text-sm">Dye Ratata development D-VPS</p>
-        </div>
-      </div>
-      <div
-        class="flex items-center justify-between text-white space-x-2 mb-2 md:mb-0 px-1 md:px-0"
-      >
-        <div class="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5 mr-2 text-green-500"
-          >
-            <rect x="4" y="4" width="16" height="16" rx="2"></rect>
-            <rect x="9" y="9" width="6" height="6"></rect>
-            <path d="M15 2v2"></path>
-            <path d="M15 20v2"></path>
-            <path d="M2 15h2"></path>
-            <path d="M2 9h2"></path>
-            <path d="M20 15h2"></path>
-            <path d="M20 9h2"></path>
-            <path d="M9 2v2"></path>
-            <path d="M9 20v2"></path>
-          </svg>
-          <span>
-            <p class="text-zinc-400 text-xs font-bold">CPU</p>
-            20%
-          </span>
-        </div>
-        <div class="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5 mr-2 text-yellow-500"
-          >
-            <path d="M6 19v-3"></path>
-            <path d="M10 19v-3"></path>
-            <path d="M14 19v-3"></path>
-            <path d="M18 19v-3"></path>
-            <path d="M8 11V9"></path>
-            <path d="M16 11V9"></path>
-            <path d="M12 11V9"></path>
-            <path d="M2 15h20"></path>
-            <path
-              d="M2 7a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v1.1a2 2 0 0 0 0 3.837V17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-5.1a2 2 0 0 0 0-3.837Z"
-            ></path>
-          </svg>
-          <span>
-            <p class="text-zinc-400 text-xs font-bold">Memory</p>
-            151mb
-          </span>
-        </div>
-        <div class="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5 mr-2 text-red-500"
-          >
-            <line x1="22" x2="2" y1="12" y2="12"></line>
-            <path
-              d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
-            ></path>
-            <line x1="6" x2="6.01" y1="16" y2="16"></line>
-            <line x1="10" x2="10.01" y1="16" y2="16"></line>
-          </svg>
-          <span>
-            <p class="text-zinc-400 text-xs font-bold">Disk</p>
-            1.2gb
-          </span>
-        </div>
-        <button
-          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border hover:text-accent-foreground h-10 px-4 py-2 bg-blue-500 border-blue-500 hover:bg-blue-600 hover:border-blue-600 transition-colors duration-200 ease-in-out"
-        >
-          Manage
-        </button>
-      </div>
+    <div class="grid gap-4 grid-cols-1">
+      <InstanceCard />
+      <InstanceCard />
+      <InstanceCard />
     </div>
   </main>
 </div>
